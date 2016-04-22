14378f705ac727397e259cdbad88ca8486d03fce
David Rodrigues <david.ndh@gmail.com>
2016-04-23 00:22:58 +0100

    Add rex_valueChanged to UISwitch

--- Source/UIKit/UISwitch.swift
+++ Source/UIKit/UISwitch.swift
@@ -21,7 +21,35 @@ extension UISwitch {
 
         return property
     }
-    
+
+    /// Exposes a property that binds an action into a control's value changed event.
+    ///
+    /// - Important: This also binds the enabled state of the action to the `rex_enabled`
+    /// property on the control.
+    ///
+    /// - Warning: Since iOS 7, `UISwitch` can trigger multiple `.ValueChanged` notifications
+    /// even when there's no change of value. This property observes internally changes on the
+    /// `on` property and manually triggers the action when there's in fact a change of value.
+    /// Because of this, the action will not be set as target of the control.
+    /// [rdar://14485185](https://openradar.appspot.com/14485185)
+    ///
+    public var rex_valueChanged: MutableProperty<CocoaAction> {
+        return associatedObject(self, key: &valueChangedKey) { host in
+            let initial = CocoaAction.rex_disabled
+            let property = MutableProperty(initial)
+
+            host.rex_on
+                .signal
+                .skipRepeats()
+                .combineLatestWith(property.signal)
+                .observeNext { [weak host] _, action in action.execute(host) }
+
+            host.rex_enabled <~ property.producer.flatMap(.Latest) { $0.rex_enabledProducer }
+
+            return property
+        }
+    }
 }
 
 private var onKey: UInt8 = 0
+private var valueChangedKey: UInt8 = 0

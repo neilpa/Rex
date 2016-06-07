ab0a35482b4cba893b98116f3bffdfe32c0acff5
Andrew Ware <andrew.ware@gordon.edu>
2016-06-07 15:15:04 -0400

    Move code into correct file

--- Source/UIKit/UITableViewCell.swift
+++ Source/UIKit/UITableViewCell.swift
@@ -3,9 +3,20 @@
 //  Rex
 //
 //  Created by David Rodrigues on 19/04/16.
+//  modified by Andrew Ware on 6/7/16.
 //  Copyright © 2016 Neil Pankey. All rights reserved.
 //
 
+import ReactiveCocoa
 import UIKit
 
-extension UITableViewCell: Reusable {}
+extension UITableViewCell: Reusable {
+    
+    /// Wraps a UITableViewCell's `userInteractionEnabled` value in a bindable property.
+    public var rex_userInteractionEnabled: MutableProperty<Bool> {
+        return associatedProperty(self, key: &userInteractionEnabledKey, initial: { $0.userInteractionEnabled }, setter: { $0.userInteractionEnabled = $1 })
+    }
+    
+}
+
+private var userInteractionEnabledKey: UInt8 = 0

9d57745fae6751e3012123355caf58c2bd73683c
Andrew Ware <andrew.ware@gordon.edu>
2016-06-07 16:17:07 -0400

    Revert UITableViewCell.swift to original

--- Source/UIKit/UITableViewCell.swift
+++ Source/UIKit/UITableViewCell.swift
@@ -10,13 +10,4 @@
 import ReactiveCocoa
 import UIKit
 
-extension UITableViewCell: Reusable {
-    
-    /// Wraps a UITableViewCell's `userInteractionEnabled` value in a bindable property.
-    public var rex_userInteractionEnabled: MutableProperty<Bool> {
-        return associatedProperty(self, key: &userInteractionEnabledKey, initial: { $0.userInteractionEnabled }, setter: { $0.userInteractionEnabled = $1 })
-    }
-    
-}
-
-private var userInteractionEnabledKey: UInt8 = 0
+extension UITableViewCell: Reusable {}
